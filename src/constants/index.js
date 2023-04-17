60b1a4349554a97ff493be75879eb04664f49404
Fanqiang Meng <fanmeng0417@gmail.com>
2023-04-17 14:33:07 -0700

    minor update

--- src/constants/index.js
+++ src/constants/index.js
@@ -37,7 +37,7 @@ import {
   ];
 
   const overview = {
-    text: "I'm a skilled software developer with experience in TypeScript and JavaScript, and expertise in frameworks like React, Node.js, and Three.js. I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!"
+    text: "I'm a skilled React developer with experience in JavaScript and expertise in frameworks like React, and Three.js. I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!"
   }
 
   const projectOverView = {
